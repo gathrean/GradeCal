7d0d22babb9dbb0be7b54aa43b689a25928762bd
Gathrean Dela Cruz <gathrean@icloud.com>
2023-06-19 21:10:28 -0700

    Fixed hamburger issues, now sidebar slides over when clicked.

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,22 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 function Navbar() {
-  return (
-    <nav className="sidebar">
-      <ul>
-        <li>
-          <Link to="/courses">Courses</Link>
-        </li>
-        <li>
-          <Link to="/profile">Profile</Link>
-        </li>
-        <li>
-          <Link to="/settings">Settings</Link>
-        </li>
-      </ul>
-    </nav>
-  );
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+    const toggleSidebar = () => {
+        setIsSidebarOpen(!isSidebarOpen);
+    };
+
+    return (
+        <>
+            <nav className={`sidebar ${isSidebarOpen ? 'open' : ''}`}>
+                <ul>
+                    <li>
+                        <Link to="/courses">Courses</Link>
+                    </li>
+                    <li>
+                        <Link to="/profile">Profile</Link>
+                    </li>
+                    <li>
+                        <Link to="/settings">Settings</Link>
+                    </li>
+                </ul>
+            </nav>
+            <div className="hamburger" onClick={toggleSidebar}>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </>
+    );
 }
 
 export default Navbar;
