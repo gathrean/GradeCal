1f7b2b55da22cb558c76c2553e165f22697942e8
Gathrean Dela Cruz <gathrean@icloud.com>
2023-06-20 18:21:49 -0700

    Added lines for environment variables

--- src/server/Server.js
+++ src/server/Server.js
@@ -1,17 +1,29 @@
+// Server-side  of OrcaSwipe and app's main entry point
+
+// Import required modules
 require('dotenv').config();
 
-const express = require('express');
-const mongoose = require('mongoose');
-const authRoutes = require('./routes/auth');
+const express = require('express');               // Import express
+const mongoose = require('mongoose');             // Import mongoose
+const authRoutes = require('./routes/auth');      // Import auth routes
 
-const app = express();
-const port = process.env.PORT || 5000;
+const app = express();                            // Create an express app
+const port = process.env.PORT || 5000;            // Set the port
+
+// Environment variables
+const mongodb_user = process.env.MONGODB_USER;
+const mongodb_password = process.env.MONGODB_PASSWORD;
+const mongodb_cluster = process.env.MONGODB_CLUSTER;
+const mongodb_database = process.env.MONGODB_DATABASE;
+const node_session_secret = process.env.NODE_SESSION_SECRET;
+const mongodb_session_secret = process.env.MONGODB_SESSION_SECRET;
+
+const dbURI = process.env.DB_URI;
 
 // Middleware
 app.use(express.json()); // Parse JSON request bodies
 
 // Connect to MongoDB
-const dbURI = process.env.DB_URI; // Use the environment variable
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log('Connected to MongoDB');
@@ -23,3 +35,5 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
 
 // Routes
 app.use('/api', authRoutes);
+
+module.exports = app;
